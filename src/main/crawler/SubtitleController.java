ccd790320720b2636fb8382fde2cad8c36dc3d09
Geza <radicsge@gmail.com>
2014-11-20 21:59:21 +0800

    remove url

--- src/main/crawler/SubtitleController.java
+++ src/main/crawler/SubtitleController.java
@@ -20,30 +20,17 @@ public class SubtitleController {
 		config.setPolitenessDelay(10000);
 		config.setUserAgentString("Language experiment (http://github.com/holdfenytolvaj/simlanpro)");
 
-		/*
-		 * Instantiate the controller for this crawl.
-		 */
 		PageFetcher pageFetcher = new PageFetcher(config);
 		RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
 		robotstxtConfig.setEnabled(false);
 		RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
 		CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
 
-		/*
-		 * For each crawl, you need to add some seed urls. These are the first
-		 * URLs that are fetched and then the crawler starts following links
-		 * which are found in these pages
-		 */
-
-		//controller.addSeed("http://www.opensubtitles.org/en/search/sublanguageid-eng/moviename-u");
+		//url removed
 		for (int i = 40; i <= 40; i += 40) {//1914
-			controller.addSeed("http://www.opensubtitles.org/en/search/sublanguageid-eng/moviename-u/offset-" + i + "");
+			controller.addSeed("http://" + i);
 		}
 
-		/*
-		 * Start the crawl. This is a blocking operation, meaning that your code
-		 * will reach the line after this only when crawling is finished.
-		 */
 		SubtitleCrawler.configure(config.getCrawlStorageFolder());
 		controller.start(SubtitleCrawler.class, numberOfCrawlers);
 
